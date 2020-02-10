b99efcd8eb2dc8cfb06c61849903d6b70320b93a
Arlen Li <Arlen.Li@myob.com>
2020-02-11 07:56:00 +1100

    Grains

--- grains/Grains.cs
+++ grains/Grains.cs
@@ -2,13 +2,21 @@
 
 public static class Grains
 {
+    public static readonly int SizeOfChessBoard = 64;
     public static ulong Square(int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if(n < 1 || n > SizeOfChessBoard)
+            throw new ArgumentOutOfRangeException();
+        var result = (ulong)Math.Pow(2, (double)(n-1));
+        return result;
     }
 
     public static ulong Total()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        ulong total = 0;
+        for(int i = 1; i <= SizeOfChessBoard; i ++) {
+            total += Square(i);
+        }
+        return total;
     }
 }
